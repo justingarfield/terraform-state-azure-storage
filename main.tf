8591761962f81608cc5659f8e3c5617dba55e714
justingarfield <accounts@jgarfield.com>
2022-04-05 13:14:07 -0400

    Cleaned up a bit

--- main.tf
+++ main.tf
@@ -5,7 +5,10 @@ terraform {
       version = "~> 3.0.2"
     }
   }
-
+  
+  # Note: These values must be populated with whatever names were chosen for your Resource Group and Storage Account.
+  #       Variables cannot be used here, as this affects `terraform init`, not `plan` or `apply`
+  #       Only uncomment the lines below AFTER the first set of steps, or else you'll get errors.
   #backend "azurerm" {
   #  resource_group_name  = "my-tf-state"
   #  storage_account_name = "mytfstatestorage"
@@ -15,6 +18,7 @@ terraform {
 }
 
 locals {
+  location = "East US 2"
   tfVersion = "1.1.7" # The version of Terraform being used to provision resources. Used for tagging.
   tags = {
     terraform = "${local.tfVersion}"
@@ -28,14 +32,14 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "resource_group" {
   name     = "my-tf-state"
-  location = "East US 2"
+  location = local.location
   tags     = local.tags
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = "mytfstatestorage"
   resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = "East US 2"
+  location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
